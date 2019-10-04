254199b2be42606cfa231b449a72d9fccbd1ffe6
Thibault Glandais <thibault.glandais@outlook.fr>
2019-10-04 14:39:54 +0700

    Add history, add map

---
+++ js/map.js
@@ -0,0 +1,27 @@
+const map = L.map('map').setView([38.398759, 11.608872], 3);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c']
+}).addTo(map)
+
+
+const myURL = jQuery('script[src$="map.js"]').attr('src').replace('map.js', '').replace('js/', '')
+console.log(myURL)
+for (var i = 0; i < markers.length; ++i) {
+  var text = '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a><br/>'
+  if (markers[i].desc)
+    text += '<p>' + markers[i].desc + '</p>'
+
+  const icon = L.icon({
+    iconUrl: myURL + 'img/icons/' + (markers[i].icon || 'E2R5.png'),
+    iconRetinaUrl: myURL + 'img/icons/' + (markers[i].icon || 'E2R5.png'),
+    iconSize: [29, 24],
+    iconAnchor: [9, 21],
+    popupAnchor: [0, -14]
+  })
+
+  L.marker([markers[i].lat, markers[i].lng], { icon: icon })
+    .bindPopup(text)
+    .addTo(map);
+}
---
+++ js/members.js
@@ -0,0 +1,37 @@
+markers = [
+  {
+    "name": "E2R5",
+    "url": "https://e2r5-corp.github.io/E2R5/",
+    "lat": 48.896405,
+    "lng": 2.318612
+  },
+  {
+    "name": "Glandais Thibault",
+    "desc": "FullStack Developper at <a href='https://www.dirox.net' target='_blank'>Dirox</a> in HCMC, Vietnam",
+    "icon": "tglandais.png",
+    "url": "https://www.linkedin.com/in/thibault-glandais/",
+    "lat": 10.792753,
+    "lng": 106.718037
+  },
+  {
+    "name": "Anroche",
+    "url": "https://www.linkedin.com/in/antoineroche42/",
+    "lat": 48.902943,
+    "lng": 2.320196
+  },
+  {
+    "name": "Giro",
+    "desc": "Founding <a href='https://learneed.com/' target='_blank'>Learneed.com</a>",
+    "url": "https://www.linkedin.com/in/thmsgiraud/",
+    "lat": 35.881105,
+    "lng": 14.448138
+  },
+  {
+    "name": "Titus",
+    "desc": "CTO at <a href='https://www.hive.fi' target='_blank'>Hive Helsinki</a>",
+    "icon": "titus.png",
+    "url": "https://www.linkedin.com/in/jordane-gengo-388626137/",
+    "lat": 60.180962,
+    "lng": 24.958573
+  }
+];
